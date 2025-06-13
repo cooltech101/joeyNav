03d3dcf5e43a979c2e95854d84c1551ef2867d52
cooltech101 <62325518+cooltech101@users.noreply.github.com>
2025-06-13 15:14:59 +0800

    Add files via upload

---
+++ install_visualnav.sh
@@ -0,0 +1,41 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+# Setup sources
+sudo apt-get install -y software-properties-common curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Install ROS2 Humble and Python3
+sudo apt-get update
+sudo apt-get install -y \
+    ros-humble-desktop \
+    python3-colcon-common-extensions \
+    python3-rosdep \
+    python3-argcomplete
+
+# Initialize rosdep
+sudo rosdep init && rosdep update
+
+# Install git and python3-pip
+sudo apt-get install -y git python3-pip
+pip3 install gdown
+
+# Install Poetry ..first time installation might fail, need to add Poetry to PATH, then reload terminal
+curl -sSL https://install.python-poetry.org | python3 -
+
+# Clone the VisualNav-Transformer repository
+git clone https://github.com/cooltech101/joeyNav.git ~/vnav-transformer
+
+# Set the working directory
+cd ~/vnav-transformer
+
+# Install dependencies using Poetry
+poetry install --no-interaction --no-ansi
+
+# Install poetry shell plugin
+poetry self add poetry-plugin-shell
+
+
+mkdir ~/vnav-transformer/model_weights
+python3 -m gdown https://drive.google.com/uc?id=1YJhkkMJAYOiKNyCaelbS_alpUpAJsOUb -O ~/vnav-transformer/model_weights/nomad.pth
